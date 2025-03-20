50c792746fa8245148531192669824a3038a47f3
Shivendra Patel <130781766+Shivaay3690@users.noreply.github.com>
2025-03-20 16:49:13 +0000

    update in app file

--- khatabook/app.js
+++ khatabook/app.js
@@ -1,17 +1,22 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const fs = require('fs')
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
 
-
-app.set("view engine" , "ejs")
-app.use(express.json())
+app.set("view engine" , "ejs");
+app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/' , (req , res) =>{
-    res.render("index")
+app.get('/' , (req , res) => {
+     fs.readdir(`./hisaab` , function(err , files){
+        if(err) return res.status(500).send(err);
+        res.render("index" , {files: files});
+     });
+});
 
+app.get("/create" , function(req , res){
+    res.render("create")
 })
-
+ 
 app.listen(3000)
